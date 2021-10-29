1f68a986cee6edc2c4105736e742e3f5ce90fa9f
SongMinJae <smj7172@naver.com>
2021-10-29 07:06:27 +0000

    make ipy

--- arguments.py
+++ arguments.py
@@ -9,7 +9,7 @@ class ModelArguments:
     """
 
     model_name_or_path: str = field(
-        default="klue/bert-base",
+        default="klue/roberta-large",
         metadata={
             "help": "Path to pretrained model or model identifier from huggingface.co/models"
         },
